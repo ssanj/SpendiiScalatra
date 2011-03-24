7118dfdb2e6f42b55076145ff4452c78932f9fb8
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2011-03-24 16:58:48 +1000

    added login

--- src/main/scala/SpendiiServlet.scala
+++ src/main/scala/SpendiiServlet.scala
@@ -5,6 +5,8 @@ import scalate.ScalateSupport
 
 class SpendiiServlet extends ScalatraServlet with ScalateSupport {
 
+  import scala.xml.NodeSeq
+
   object Paths {
     val loginTemplate = "/WEB-INF/scalate/templates/login.scaml"
   }
@@ -16,7 +18,38 @@ class SpendiiServlet extends ScalatraServlet with ScalateSupport {
       </body>
     </html>
   }
+
   get("/") {
+    contentType = "text/html"
+    templateEngine.layout(Paths.loginTemplate)
+  }
+
+  post("/") {
+    case class User(username:String, password:String)
+    val validation = for {
+     u <- params.get("username")
+     p <- params.get("password")
+    } yield User(u, p)
+
+    fold(validation)(gotoLogin("The supplied username and password were incorrect.")) { user =>
+      if (user.username == "admin" && user.password == "admin2010") successfulLogin
+      else gotoLogin("There was problem retrieving your user credentials")
+    }
+  }
+
+  private def successfulLogin: Any = {
+    <html>
+      <body>
+        <h1>You logged in Successfuly</h1>
+      </body>
+    </html>
+  }
+  private def fold[T, R](op:Option[T])(none: => R)(some: T => R): R = op match {
+    case Some(value) => some(value)
+    case None => none
+  }
+
+  private def gotoLogin(message:String): Any = {
     contentType = "text/html"
     templateEngine.layout(Paths.loginTemplate)
   }
